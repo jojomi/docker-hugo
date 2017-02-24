2d99c257dd637206cf9452fa73367759f6fe9d78
Anton Lugovoi <a_lugovoi@gcore.lu>
2017-02-24 17:10:21 +0300

    removed pid 1 zombie issue, removed run.sh loop and sleep timers since they are not needed

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,12 @@ FROM alpine:latest
 MAINTAINER Johannes Mitlmeier <dev.jojomi@yahoo.com>
 
 ENV HUGO_VERSION=0.18.1
-RUN apk add --update wget ca-certificates && \
-  cd /tmp/ && \
-  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz && \
-  tar xzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz && \
-  rm -r hugo_${HUGO_VERSION}_Linux-64bit.tar.gz && \
-  mv hugo*/hugo* /usr/bin/hugo && \
-  apk del wget ca-certificates && \
-  rm /var/cache/apk/*
+
+ADD https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz /tmp
+RUN tar -xf /tmp/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -C /tmp \
+    && mkdir -p /usr/local/sbin \
+    && mv /tmp/hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 /usr/local/sbin/hugo \
+    && rm -rf /tmp/hugo_${HUGO_VERSION}_linux_amd64
 
 COPY ./run.sh /run.sh
 
--- run.sh
+++ run.sh
@@ -1,29 +1,25 @@
 #!/usr/bin/env sh
 
-WATCH="${HUGO_WATCH:=false}"
-SLEEP="${HUGO_REFRESH_TIME:=-1}"
-echo "HUGO_WATCH:" $WATCH
-echo "HUGO_REFRESH_TIME:" $HUGO_REFRESH_TIME
-echo "HUGO_THEME:" $HUGO_THEME
-echo "HUGO_BASEURL" $HUGO_BASEURL
-echo "ARGS" $@
+echo -e 'HUGO_WATCH: \t'   ${HUGO_WATCH:-false}
+echo -e 'HUGO_THEME: \t'   ${HUGO_THEME:-undefined}
+echo -e 'HUGO_BASEURL: \t' ${HUGO_BASEURL:-undefined}
+echo -e 'ARGS: \t' $@
 
-HUGO=/usr/bin/hugo
-
-while [ true ]
-do
-    if [[ $HUGO_WATCH != 'false' ]]; then
-	    echo "Watching..."
-        $HUGO server --watch=true --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" --bind="0.0.0.0" "$@" || exit 1
-    else
-	    echo "Building one time..."
-        $HUGO --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" "$@" || exit 1
-    fi
-
-    if [[ $HUGO_REFRESH_TIME == -1 ]]; then
-        exit 0
-    fi
-    echo "Sleeping for $HUGO_REFRESH_TIME seconds..."
-    sleep $SLEEP
-done
-
+if [[ $HUGO_WATCH != 'false' ]]; then
+    echo "Watching...";
+    exec hugo \
+            server \
+                --watch=true \
+                --source="/src" \
+                --theme="$HUGO_THEME" \
+                --destination="/output" \
+                --baseUrl="$HUGO_BASEURL" \
+                --bind="0.0.0.0" "$@";
+else
+    echo "Building one time...";
+    exec hugo \
+            --source="/src" \
+            --theme="$HUGO_THEME" \
+            --destination="/output" \
+            --baseUrl="$HUGO_BASEURL" "$@";
+fi
