9658be9146714ec5eb1782036ba8a30840ba4018
Travis Hall <thall@vmware.com>
2018-06-05 10:42:31 -0700

    Removed server command
    Hosting is intended to be passed through to Nginx running against
    the static destination. Setting watch just sets the watch flag.
    
    Refactored the run script

--- run.sh
+++ run.sh
@@ -1,31 +1,27 @@
 #!/bin/sh
 
-WATCH="${HUGO_WATCH:=false}"
-SLEEP="${HUGO_REFRESH_TIME:=-1}"
+HUGO_WATCH="${HUGO_WATCH:=false}"
 HUGO_SRC="${HUGO_SRC:=/src}"
 HUGO_DESTINATION="${HUGO_DESTINATION:=/output}"
-HUGO_APPEND_PORT="${HUGO_APPEND_PORT:=false}"
-echo "HUGO_WATCH:" $WATCH
-echo "HUGO_REFRESH_TIME:" $HUGO_REFRESH_TIME
-echo "HUGO_BASEURL" $HUGO_BASEURL
+echo "HUGO_SRC:" $HUGO_SRC
+echo "HUGO_DESTINATION" $HUGO_DESTINATION
+echo "HUGO_BASEURL:" $HUGO_BASEURL
+echo "HUGO_WATCH:" $HUGO_WATCH
 echo "ARGS" $@
 
 HUGO=/usr/local/sbin/hugo
 echo "Hugo path: $HUGO"
 
-while [ true ]
-do
-    if [[ $HUGO_WATCH != 'false' ]]; then
-	    echo "Watching..."
-        $HUGO server --watch=true --source="$HUGO_SRC" --destination="$HUGO_DESTINATION" --baseURL="$HUGO_BASEURL" --appendPort="$HUGO_APPEND_PORT" --disableFastRender --bind="0.0.0.0" "$@" || exit 1
-    else
-	    echo "Building one time..."
-        $HUGO --source="$HUGO_SRC" --destination="$HUGO_DESTINATION" --baseURL="$HUGO_BASEURL" --appendPort="$HUGO_APPEND_PORT" "$@" || exit 1
-    fi
+arguments=(
+  --source="$HUGO_SRC"
+  --destination="$HUGO_DESTINATION"
+  --baseURL="$HUGO_BASEURL"
+  --cleanDestinationDir
+)
 
-    if [[ $HUGO_REFRESH_TIME == -1 ]]; then
-        exit 0
-    fi
-    echo "Sleeping for $HUGO_REFRESH_TIME seconds..."
-    sleep $SLEEP
-done
+if [[ $HUGO_WATCH != 'false' ]]; then
+  arguments+=('--watch')
+fi
+
+$HUGO "${arguments[@]}" "$@" || exit 1
+exit 0
