d0f86220a1f2f592f99d1471906d1fa6eb1e7de2
Rich Tong <rich@tongfamily.com>
2021-02-22 17:12:47 -0800

    Fix URL for docker hub

--- update.sh
+++ update.sh
@@ -61,5 +61,5 @@ then
     git push --follow-tags
   # open hub.docker.com
   "$OPEN" "https://hub.docker.com/r/$DOCKER_ORG/hugo/builds/" > /dev/null
-  "$OPEN" "https://hub.docker.com/r/$DOCKER_ORG/hugo/~/settings/automated-builds/" > /dev/null
+  "$OPEN" "https://hub.docker.com/repository/docker/$DOCKER_ORG/hugo" /dev/null
 fi
